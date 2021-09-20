258c87a51f25b5d3b96b50c0c1a5c9d7466a1e6e
Eduardo Guzmán Hermandez <eduardo.guzman@placetopay.com>
2021-09-20 17:17:27 -0500

    Adds complex test cases

--- tests/Feature/CheckMutantTest.php
+++ tests/Feature/CheckMutantTest.php
@@ -6,31 +6,127 @@ use Tests\TestCase;
 
 class CheckMutantTest extends TestCase
 {
-    public function test_is_mutant()
+    public function mutantsDataSet()
     {
-        $response = $this->post('/mutant', ["dna" => [
-            "ATGCGA",
-            "CAGTGC",
-            "TTATGT",
-            "AGAAGG",
-            "CCCCTA",
-            "TCACTG",
-        ]]);
-
-        $response->assertOk();
+        return [
+            '2 rows' => [
+                "AAAA",
+                "TTTT",
+                "AGCT",
+                "GGAT",
+            ],
+            '2 columns' => [
+                "AGGC",
+                "ACGA",
+                "AGGT",
+                "AAGG",
+            ],
+            '3 diagonals left to right' => [
+                "ATGCTA",
+                "GAGCGG",
+                "CTAGCA",
+                "TCGAGA",
+                "TTCAGG",
+                "TTACGG",
+            ],
+            '2 diagonals right to left' => [
+                "ATGCTA",
+                "GAGTGG",
+                "CTTGCA",
+                "TTGCGA",
+                "TTCAGC",
+                "TCACGG",
+            ],
+            '1 of each' => [
+                "ATGCTA",
+                "GAGTAG",
+                "TATACA",
+                "TAAAAA",
+                "TTCAGC",
+                "TCGCAG",
+            ],
+            'doc test' => [
+                "ATGCGA",
+                "CAGTGC",
+                "TTATGT",
+                "AGAAGG",
+                "CCCCTA",
+                "TCACTG",
+            ],
+        ];
     }
 
-    public function test_is_not_mutant()
+    /**
+     * @dataProvider mutantsDataSet
+     */
+    public function test_is_mutant(...$dna)
     {
-        $response = $this->post('/mutant', ["dna" => [
-            "ATGCGA",
-            "CAGTGC",
-            "TTATTT",
-            "AGACGG",
-            "GCGTCA",
-            "TCACTG",
-        ]]);
-
-        $response->assertForbidden();
+        $this->post('/mutant', ["dna" => $dna])->assertOk();
     }
+
+    public function notMutantsDataSet()
+    {
+        return [
+            '1x1' => [[
+                "A",
+            ]],
+            '2x2' => [[
+                "AT",
+                "TT",
+            ]],
+            '3x3' => [[
+                "AGC",
+                "TGC",
+                "TTG",
+            ]],
+            '4x3' => [[
+                "TGC",
+                "TGC",
+                "TTG",
+                "TTG",
+            ]],
+            '3x4' => [[
+                "TGCG",
+                "TGCC",
+                "TTTT",
+            ]],
+            'nothing found' => [[
+                "ATAA",
+                "TTGT",
+                "AGCT",
+                "GGAT",
+            ]],
+            'Bad Chars' => [[
+                "AXAA",
+                "TTGT",
+                "AGXT",
+                "GGAT",
+            ]],
+            'doc Test' => [[
+                "ATGCGA",
+                "CAGTGC",
+                "TTATTT",
+                "AGACGG",
+                "GCGTCA",
+                "TCACTG",
+            ]],
+            'not array' => [12356],
+            'null' => [null],
+            'empty string' => [''],
+            'empty array' => [[]],
+            'bad lengths' => [[
+                "A",
+                "BB",
+            ]],
+        ];
+    }
+
+    /**
+     * @dataProvider notMutantsDataSet
+     */
+    public function test_is_not_mutant($dna)
+    {
+        $this->post('/mutant', ["dna" => $dna])->assertForbidden();
+    }
+
 }
