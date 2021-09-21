89201b1b42bbf882490ef1d7362cf7fea00ae69f
Eduardo Guzmán Hermandez <eduarguzher@gmail.com>
2021-09-20 21:12:27 -0500

    Moves routes to api

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,1 @@
 <?php
-
-use App\Http\Controllers\MutantController;
-use App\Http\Controllers\StatsController;
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::post('/mutant', [MutantController::class, 'store']);
-Route::get('/stats', [StatsController::class, 'index']);
